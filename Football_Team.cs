a3909e2cf1ce2f78935bd7ab28e5ed0a5ab0eb09
Elena <VoronovaElena057@gmail.com>
2020-03-04 22:42:36 +0300

    Change cs file

--- Football_Team.cs
+++ Football_Team.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
@@ -10,28 +7,24 @@ namespace ConsoleApplication1
     {
         
 		public class TeamMember
-       {
-        
+       {   
             public string Name{get; set;}
-            public int Age{get; set;}
 			public int Luck{get; set;}
-			public Random _age=new Random();// Почему у всех одинаковый возраст если напеменную _age не делать статической?? 
 			public static Random r = new Random();
 			
             public TeamMember()
             {
 			 Luck=r.Next(0, 100);
-			 Age=_age.Next(18,45);	
 			}
 
-            public TeamMember(string Name):this()
+            public TeamMember(string name):this()
             {
-                this.Name = Name;
+                Name = name;
             }
 			
 			public virtual void Info()
 			{
-			 Console.WriteLine("This team member has lucky rate: {0}, and his age: {1}",Luck,Age);
+			 Console.WriteLine("This team member has lucky rate: {0}}",Luck);
 			}
         }
 
@@ -44,7 +37,7 @@ namespace ConsoleApplication1
 			
 			public override void Info()
 			{
-			 Console.WriteLine("Goalkeeper age: {0}, Name: {1}, skill:{2}",Age,Name,skill);
+			 Console.WriteLine("Goalkeeper Name: {0}, skill:{1}",Name,skill);
 			}
         }
 
@@ -56,7 +49,7 @@ namespace ConsoleApplication1
 			
 			public override void Info()
 			{
-			 Console.WriteLine("Defender age: {0}, Name: {1}, defence: {2}",Age,Name,defence);
+			 Console.WriteLine("Defender  Name: {0}, defence: {1}",Name,defence);
 			}
         }
 		
@@ -68,7 +61,7 @@ namespace ConsoleApplication1
 			
 			public override void Info()
 			{
-			 Console.WriteLine("Midfield aga: {0}, Name: {1}, speed: {2}",Age,Name,speed);
+			 Console.WriteLine("Midfield  Name: {0}, speed: {1}",Name,speed);
 			}
         }
 		
@@ -80,7 +73,7 @@ namespace ConsoleApplication1
 			
 			public override void Info()
 			{
-			 Console.WriteLine("Midfield aga: {0}, Name: {1}, attack: {2}",Age,Name,attack);
+			 Console.WriteLine("Forward  Name: {0}, attack: {1}",Name,attack);
 			}
         }
 
@@ -91,22 +84,30 @@ namespace ConsoleApplication1
 		   
            public Dictionary<string ,TeamMember> spisok= new Dictionary<string, TeamMember>();//список игроков под номерами teamNumber
 			
-            public Team(string TeamName, string[] teamNumber)//конструктор писваивает имя команде и добавляет разных гроков в команду
+            public Team(string teamName, string[] teamNumber)//конструктор писваивает имя команде и добавляет разных гроков в команду
             {
-                this.TeamName = TeamName;
+                TeamName = teamName;
 				
-                for (int i=0; i< 11; i++)
-                {
-					if(i==0)
-                    spisok.Add(teamNumber[i],new Goalkeeper());
-					else if(i>0&&i<=4)
-					spisok.Add(teamNumber[i],new Defender());
-                    else if(i>4&&i<=8)
-					spisok.Add(teamNumber[i],new Midfield());
-					else if(i>8&&i<=10)
-					spisok.Add(teamNumber[i],new Forward());
+				for (int i=0; i< 11; i++)
+                {		
+                    spisok.Add(teamNumber[i], MakePlayer(i));	
                 }
+                
             }
+			
+			public TeamMember MakePlayer(int i)
+			{
+					if (i == 0)
+                    return new Goalkeeper();
+                else if (i > 0 && i <= 4)
+                    return new Defender();
+                else if (i > 4 && i <= 8)
+                    return new Midfield();
+                else if (i > 8 && i <= 10)
+                    return new Forward(); 
+				else 
+					return null;
+			}
 			
 			public void addNames(string [] names)
 			{
@@ -127,6 +128,16 @@ namespace ConsoleApplication1
                 return teamLuck/11 ; 
             }
 			
+		public  void Luck()
+		{
+		  Console.WriteLine("Average luck {0} of team {1} \n",this.AverageLuck(),this.TeamName);
+		  Console.WriteLine("Luck of each member of the {0} team:",this.TeamName);	
+		  foreach(string teamKey in this.spisok.Keys)
+		  {
+		   Console.WriteLine("Player number: {0,-3} luck: {1}",teamKey,this.spisok[teamKey].Luck);
+		  }
+		}
+			
 			public void TeamInfo()
 			{
 				Console.WriteLine("Name of the football team: {0}", TeamName);
@@ -135,29 +146,55 @@ namespace ConsoleApplication1
 			}
         }
 
-
-        public static void  GameWinner(Team a, Team b)//метод который определяет исход игры
+       class Game
+      {
+	    Team a;
+	    Team b;
+		public string yourBet;
+		//Message message;
+		//public event Message Bet;
+		   
+		   public Game(Team a, Team b)
+		   {
+		    this.a=a;
+			this.b=b;   
+		   }
+		   
+		   public void YourBet(int bet)
+		   {
+		    if (bet==1)
+			     yourBet="Arsenal"; 	
+			else if(bet==2)
+			     yourBet="Chelsea";
+		    else
+			Console.WriteLine("You enter a wrong character. Your bet on this match will not be accepted.");	
+		   }		   		 
+	
+        public  void  GameWinner()//метод который определяет исход игры
         {
             if (a.AverageLuck() > (b.AverageLuck()*1.1))
-                Console.WriteLine("Team \"{0}\" is winner \n",a.TeamName);
+			{
+               Console.WriteLine("Team \"{0}\" is winner \n",a.TeamName);
+				if(yourBet==a.TeamName)
+				Console.WriteLine("Your bet won!");
+				else
+				Console.WriteLine("Your bet lost(");	
+			}
             else if ((a.AverageLuck()*1.1) < b.AverageLuck())
+			{
                 Console.WriteLine("Team \"{0}\" is winner \n", b.TeamName);
+				if(yourBet==b.TeamName)
+				Console.WriteLine("Your bet won!");
+				else
+				Console.WriteLine("Your bet lost(");
+			}
             else
-                Console.WriteLine("Skore is equal \n");
-        }
-		
-		public static void Luck(Team team)
-		{
-		  Console.WriteLine("Average luck {0} of team {1} \n",team.AverageLuck(),team.TeamName);
-		  Console.WriteLine("Luck of each member of the {0} team:",team.TeamName);	
-		  foreach(string teamKey in team.spisok.Keys)
-		  {
-		   Console.WriteLine("Player number: {0,-3} luck: {1}",teamKey,team.spisok[teamKey].Luck);
-		  }
+                Console.WriteLine("Skore is equal. \n");
 		}
+      }	
 		
-		
-
+     // public delegate void Message(int bet);  
+	
       public  static void Main(string[] args)
         { 
 
@@ -166,18 +203,25 @@ namespace ConsoleApplication1
 		    string [] namesA=new string[11]{"Vasja","Vova","Petja","Igor","Dima","Sasha","Misha","Vanja","Kolja","Vlad","Sergej"};
 		    A.addNames(namesA);
 		    //A.TeamInfo();
-            //Program.Luck(A);
-		    
+            //A.Luck();
+		    //Console.WriteLine("");
 		    
 
             string[] b = new string[11] {"12", "14", "15", "2", "1", "7", "88", "11", "10", "8", "17" };
             Team B = new Team("Chelsea", b);
+		    string [] namesB=new string[11]{"Andrej","Jura","Lesha","Denis","Dima","Pasha","Slava","Nikita","Tima","Kostja","Gosha"};
 		    //B.TeamInfo();
-            //Program.Luck(B);
-
-            Program.GameWinner(A, B);
-		  
-            
+            //B.Luck();
+		    //Console.WriteLine("");
+             
+		    Console.WriteLine("Two teams are playing today: \"Arsenal\" and \"Chelsea\" \nWhich team are you betting on?");
+		    Console.WriteLine("Enter nuumber 1 or 2: \n 1)\"Arsenal\"  \n 2)\"Chelsea\" ");
+		    int bet=Convert.ToInt32(Console.ReadLine());
+		    
+		    Game G=new Game(A,B);
+		    G.YourBet(bet);
+            G.GameWinner();
+		    
         }
     }
 }
